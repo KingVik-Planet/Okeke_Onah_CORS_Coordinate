88ae92e4935b6c7dafe8de9a4db7fb263085843f
KingVik-Planet <chukwukingsley56@gmail.com>
2023-11-05 00:42:21 +0100

    Update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 col1, col2, = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.jpg", caption = "Kingsley Chika CHUKWU")
     #st.image("images/1.png", width = 300)
 
 with col2:
