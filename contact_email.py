f834d1b380bd22e590dac6218bb7f8300d4d37f2
KingVik-Planet <chukwukingsley56@gmail.com>
2023-11-05 16:06:30 +0100

    Revoked the Password

--- contact_email.py
+++ contact_email.py
@@ -5,7 +5,7 @@ def contact_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "chukwukingsley56@gmail.com" #"Your Email Address or Username Like: chukwukingsley56@gmail.com"
-    password = "riwy cigx jwnx zdrb" #mightriwy becigx likejwnx thiszdrb
+    password = "riwy cigx jwnx zdrb" #Passowrd
     reciever = "chukwukingsley56@gmail.com"
     context = ssl.create_default_context()
 
