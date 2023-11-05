bb6eb1cdf374b0b07ce52b7f190a29610ea9b2b3
KingVik-Planet <chukwukingsley56@gmail.com>
2023-11-05 15:50:25 +0100

    update

--- contact_email.py
+++ contact_email.py
@@ -4,8 +4,8 @@ import smtplib, ssl
 def contact_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = "Your Email Address or Username Like: chukwukingsley56@gmail.com"
-    password = "mightriwy becigx likejwnx thiszdrb"
+    username = "chukwukingsley56@gmail.com" #"Your Email Address or Username Like: chukwukingsley56@gmail.com"
+    password = "riwy cigx jwnx zdrb" #mightriwy becigx likejwnx thiszdrb
     reciever = "chukwukingsley56@gmail.com"
     context = ssl.create_default_context()
 
