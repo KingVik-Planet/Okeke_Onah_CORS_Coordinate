75ad94f12ee69ee731194230416a0ef6212e0f4f
KingVik-Planet <chukwukingsley56@gmail.com>
2023-11-05 15:45:14 +0100

    Delete Passord

--- contact_email.py
+++ contact_email.py
@@ -4,14 +4,11 @@ import smtplib, ssl
 def contact_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = "chukwukingsley56@gmail.com"
-    password = "riwy cigx jwnx zdrb"
+    username = "Your Email Address or Username Like: chukwukingsley56@gmail.com"
+    password = "mightriwy becigx likejwnx thiszdrb"
     reciever = "chukwukingsley56@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, reciever, message)
-
-#
-# contact_email()
