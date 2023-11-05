639a65ac3da4760f9a39a4cbc1830c86f99a2898
KingVik-Planet <chukwukingsley56@gmail.com>
2023-11-05 14:53:25 +0100

    Update

---
+++ contact_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+
+def contact_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "chukwukingsley56@gmail.com"
+    password = "riwy cigx jwnx zdrb"
+    reciever = "chukwukingsley56@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever, message)
+
+
+contact_email()
--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,11 +1,13 @@
 import streamlit as st
+from contact_email import contact_email
 
 st.header("Contact me")
 
 with st.form(key = "email_forms"):
+    user_name = st.text_input("What is Your Name: ")
     user_email = st.text_input("Enter your Email Address: ")
     message = st.text_area("Your Message: ")
+    message + "\n" + user_email
     button = st.form_submit_button("Submit")
     if button:
-        print(button)
-        print("""Your Message Has Been Submitted Successfully \nThank you, we will get back to you soonest""")
+        contact_email(message)
