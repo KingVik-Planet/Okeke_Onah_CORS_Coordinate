315df395c6b182c5ffdc3bc53e9c5eabaea6859e
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-04 13:41:47 +0100

    Update

--- pages/Services.py
+++ pages/Services.py
@@ -9,6 +9,15 @@ with col1:
     st.image("images/RS.png", caption="Remote Sensing and Geospatial Science")
     st.title("Remote Sensing and Geospatial Sciences")
 
+with col1:
+    st.subheader("1. Flood Inundation, Soil Degradation and impact at Lokoja, Kogi Enviro Nigeria Using Sentinel 1 SAR"
+                 " Data on Google Earth Cloud Computing")
+    st.image("images/Kogi.png", caption="Google Earth Engine Cloud Computing ")
+    App = "[Google Earth Engine Cloud Computing App](https://chukwukingsley56.users.earthengine.app/view/kogiflood2022)"
+    App
+    st.subheader("Abstract")
+    Text1 = " Flood has been "
+    Text1
 
 
 with col2:
