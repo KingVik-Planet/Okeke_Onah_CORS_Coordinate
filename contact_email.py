84f5cb4e1b539a11da4cef82fe6e303823b07804
KingVik-Planet <chukwukingsley56@gmail.com>
2024-02-13 22:13:46 +0100

    Update

--- contact_email.py
+++ contact_email.py
@@ -33,8 +33,7 @@ import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-from xxx import pwd
-import os
+
 
 headers= {
     "authorization": st.secrets["pwd"],
@@ -45,8 +44,8 @@ def contact_email(subject, message, attachment_content=None, attachment_name=Non
     host = "smtp.gmail.com"
     port = 465
     username = "chukwukingsley56@gmail.com"
-    password = pwd
-    # password = st.secrets["pwd"]
+    # password = pwd
+    password = st.secrets["pwd"]
     sender = username
     receiver = "chukwukingsley56@gmail.com"
     context = ssl.create_default_context()
