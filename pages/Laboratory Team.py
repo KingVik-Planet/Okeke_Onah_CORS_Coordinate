9735e9b9aa63bd47836535be30f710ed090db987
KingVik-Planet <chukwukingsley56@gmail.com>
2024-02-16 09:59:03 +0100

    Update

--- pages/Laboratory Team.py
+++ pages/Laboratory Team.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
+
 st.header("||   Meet Our Research Lab Project Team ||")
-subheader= ( """
+subheader = ("""
 Okeke_Onah GNSS Laboratory (OGL)is an online GPS data processing facility that's free to use. 
 It uses localized CORS Network that covers some state in Nigeria. OGL is capable of working with data collected from any 
 location within the network and provides differential corrections to any user within the network.
@@ -12,15 +13,12 @@ st.info(subheader)
 
 col1, col2 = st.columns(2)
 
-
 with col1:
-    st.image("images/FIO.jpg", caption = "Professor Francis I. Okeke\n Supervisor")
+    st.image("images/FIO.jpg", caption="Professor Francis I. Okeke\n Supervisor")
     about = """||[Research Gate](https://www.researchgate.net/profile/Francis-Okeke-4)||...............||[UNN - Staff Details](https://staffprofile.unn.edu.ng/profile/1854)|| """
     about
 
-
 with col2:
-    st.image("images/Onah.jpeg", caption = "Surveyor Emmanuel U. Onah\n PhD Student", width=290)
+    st.image("images/Onah.jpeg", caption="Surveyor Emmanuel U. Onah\n PhD Student", width=290)
     about = """||[Research Gate](https://www.researchgate.net/profile/Emmanuel-Udochukwu-Onah)||...............||[UNN - Staff Details](https://linkedin.com/in/kingsley-chika-chukwu-235791154/)|| """
     about
-
